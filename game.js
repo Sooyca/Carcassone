35371fe77798e9610a58995284dd4be6809c2b7e
Sooyca <bartosz.sjka@gmail.com>
2017-01-20 13:03:21 +0100

    interfejs wersja1 nie dziala

--- game.js
+++ game.js
@@ -1,3 +1,167 @@
+var http = require('http')
+var socket = require('socket.io')
+var fs = require('fs')
+var express = require('express')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser');
+var engine = require('./engine')
+
+var app = express()
+var server = http.createServer(app)
+var io = socket(server)
+var cnt = 0
+var rooms = []
+var users = {}
+
+console.log(engine.startPiece);
+
+app.set('view engine', 'ejs')
+app.set('views', './')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use( cookieParser() )
+app.use(express.static('./'))
+app.use(session(
+{
+	secret: 'dfgfsgfsgsf',
+	resave: false,
+	saveUninitialized: true
+}))
+
+
+app.get('/', (req, res) =>
+{
+	var username
+	if (!req.cookies.username)
+	{
+		username = 'Anonimowy'
+		res.cookie('username', username)
+	}
+	else
+		username = req.cookies.username
+	res.render('main', {'username': username})
+})
+
+app.get('/roomsList', (req, res) =>
+{
+	res.render('roomsList', {'rooms': rooms, 'username': req.cookies.username})
+})
+
+app.get('/changeUsername', (req, res) =>
+{
+	res.cookie('username', req.query.newUsername)
+	res.redirect(req.query.returnUrl)
+})
+
+app.get('/createRoom', (req, res) =>
+{
+	console.log('Created room: ' + req.query.name)
+
+	rooms.push({'name': req.query.name, 'players': [], 'board': board})
+	res.redirect('/rooms/' + (rooms.length - 1))
+})
+
+app.get('/rooms/:id', (req, res) =>
+{
+	var id = req.params.id
+	if (rooms[id].players.length >= 2)
+		res.redirect('/roomsList')
+	else
+		res.render('board', {'name': rooms[id].name, 'roomNo': id, 'username': req.cookies.username})
+})
+
+io.on('connection', function(socket)
+{
+    console.log('Client connected: ' + socket.id)
+    /*
+    socket.emit('id', cnt++)
+    if (cnt == 2)
+    {
+    	io.emit('start', {'id': Math.floor(2*Math.random())})
+    }
+    */
+    socket.on('newgame', function()
+    {
+    	var roomNo = users[socket.id]
+
+    	//console.log(emptyBoard)
+    	for (var i = 0; i < rooms[roomNo].players.length; i++)
+    		rooms[roomNo].players[i].emit('newgame')
+    	//io.emit('newgame')
+    })
+    socket.on('move', function(data)
+    {
+    	var roomNo = users[socket.id]
+    	rooms[roomNo].board[data.x][data.y] = data.id
+    	//console.log(emptyBoard)
+    	for (var i = 0; i < rooms[roomNo].players.length; i++)
+    		rooms[roomNo].players[i].emit('move', {'board': rooms[roomNo].board, 'id': data.id})
+    	//io.emit('move', {'id': data.id, 'x': data.x, 'y': data.y})
+    })
+	socket.on('join', function(roomNo)
+	{
+		socket.emit('id', rooms[roomNo].players.length)
+		rooms[roomNo].players.push(socket)
+		users[socket.id] = roomNo
+
+	})
+	socket.on('disconnect', function()
+	{
+		console.log('Client disconnected: ' + socket.id)
+		var roomNo = -1
+		for (var i = 0; i < rooms.length; i++)
+			for (var j = 0; j < rooms[i].players.length; j++)
+				if (rooms[i].players[j] == socket)
+				{
+					rooms[i].players.splice(j, 1)
+					roomNo = i
+				}
+		if (roomNo != -1 && rooms[roomNo].players.length == 0)
+		{
+			//rooms[roomNo].players[0].emit('newgame')
+			rooms[roomNo].board = [[-1, -1, -1],
+				 			      [-1, -1, -1],
+			    			      [-1, -1, -1]]
+			//rooms[roomNo].players[0].emit('id', rooms[roomNo].players.length - 1)
+		}
+		else if (roomNo != -1 && rooms[roomNo].players.length > 0)
+		{
+			rooms[roomNo].players[0].emit('withdrawal')
+			rooms[roomNo].players[0].emit('id', rooms[roomNo].players.length - 1)
+		}
+		delete users.socket
+	})
+})
+
+
+server.listen( process.env.PORT || 5000)
+console.log('server started')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var canvasSize = 1000
 var boardSize = 10
 var gridSize = canvasSize/boardSize
@@ -33,7 +197,7 @@ var roadStraight = new createSubPiece(road, [2])
 
 var fieldAlone = new createSubPiece(field, [])
 
-var currPiece, currX, currY, currRotation, randPiece, currPlayer 
+var currPiece, currX, currY, currRotation, randPiece, currPlayer
 var mayAddMan = false
 var myTurn = true
 currRotation = 0
@@ -60,7 +224,7 @@ function getClick(e) {
 					if(currY <= yCoo && currY + 0.5 > yCoo) // góra
 						addOwner(currPiece, boardToCanvas(currX + 0.5), boardToCanvas(currY + 0.25))
 					else if (currY + 1 <= yCoo && currY + 0.5 < yCoo) //dół
-						addOwner(currPiece, boardToCanvas(currX + 0.5), boardToCanvas(currY + 0.75))  
+						addOwner(currPiece, boardToCanvas(currX + 0.5), boardToCanvas(currY + 0.75))
 				}
 			}
 			else {
@@ -121,7 +285,7 @@ function checkAssign(piece, x, y) {
 						segments[a].parts.push(y*boardSize + x)
 						console.log(segments[a])
 					}
-				}		
+				}
 			}
 			for(var a of piece[i].piece.connections) {
 				var j = (i+a)%4
@@ -137,7 +301,7 @@ function checkAssign(piece, x, y) {
 				piece[i].assign = segments.length-1
 			}
 		}
-	}	
+	}
 }
 
 function addOwner(subPiece, x, y) {
@@ -157,7 +321,7 @@ function checkNeighbours(piece, x, y) {
 	var right = board[y*boardSize + x+1]
 	var down = board[(y+1)*boardSize + x]
 	var up = board[(y-1)*boardSize + x]
-	
+
 	var hasNeighbours = false
 
 	if(left) {
@@ -175,13 +339,13 @@ function checkNeighbours(piece, x, y) {
 	if(down) {
 		if(down[north].piece.type != piece[south].piece.type)
 			return false
-		hasNeighbours = true		
+		hasNeighbours = true
 
 	}
 	if(up) {
 		if(up[south].piece.type != piece[north].piece.type)
 			return false
-		hasNeighbours = true		
+		hasNeighbours = true
 	}
 	if(!hasNeighbours) return false
 	piece[west].neighbour = left[east]
@@ -202,7 +366,7 @@ function mergeSegments(s1, s2) {
 			}
 		}
 		if(segments[s1].parts.indexOf(a) == -1)
-			segments[s1].parts.push(a) 
+			segments[s1].parts.push(a)
 	}
 	segments[s2] = null
 	console.log("po zmergowaniu:")
@@ -224,7 +388,7 @@ function sumUp(segment) {
 
 function checkClosed(subPiece) {
 	var segment = subPiece.assign
-	if(segment.closed) return false 
+	if(segment.closed) return false
 	for (var a of segment.parts)
 		if(!a.neighbour)
 			return false
@@ -241,7 +405,7 @@ function endTurn() {
 	myTurn = false
 	for(var i = 0; i<4; i++)
 		checkClosed(currPiece[i])
-}	
+}
 
 function addPiece(piece, x, y, rotation) {
 	currPiece = new drawnPiece(piece, x, y, rotation)
@@ -272,4 +436,3 @@ function addPiece(piece, x, y, rotation) {
 	}
 	return true
 }
-
