e1c8115f0b670b75be6c9a3df5f5f99012ee0a17
Sooyca <bartosz.sjka@gmail.com>
2017-01-23 22:05:09 +0100

    users table ala insert

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ function logIn()
 function register()
 {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	  client.query("INSERT INTO users VALUES (1, Ala, 34);", function(err, result) {
+	  client.query("INSERT INTO users VALUES (1, 'Ala', 34);", function(err, result) {
 		done();
 		if (err)
 		 { console.error(err); }
