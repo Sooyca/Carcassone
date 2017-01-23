e2ea1c56ba99044f3c5c4672a7ed5819a2fd2a92
Sooyca <bartosz.sjka@gmail.com>
2017-01-23 17:17:07 +0100

    advanced security

--- game.js
+++ game.js
@@ -42,6 +42,10 @@ app.get('/db', function (request, response) {
   });
 });
 
+
+app.get('/admin', function (request, response) {
+	window.alert("Nie bądź zbyt sprytny Julan.")
+});
 app.set('view engine', 'ejs')
 app.set('views', './')
 app.use(bodyParser.urlencoded({ extended: true }))
