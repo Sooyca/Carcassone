2637139996f391363744035d4292cc320b93ab3e
Sooyca <bartosz.sjka@gmail.com>
2017-01-27 17:56:48 +0100

    register test 2.11 id ? DZIAŁAAAA

--- game.js
+++ game.js
@@ -113,6 +113,7 @@ app.post('/', (req, res) => {
                            console.log(data);
                            my_id_key = data;
                    });
+                   console.log(my_id_key);
                     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
                 	  client.query("INSERT INTO users VALUES ($1, $2, 3);", [my_id_key, dane.nazwa], function(err, result) {
                 		done();
