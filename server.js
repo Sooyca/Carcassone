bde232144300d5b6ea59a13d48439f5e5a6dacc5
Sooyca <bartosz.sjka@gmail.com>
2017-02-04 16:35:45 +0100

    logIn

--- server.js
+++ server.js
@@ -237,7 +237,7 @@ app.post('/logIn', (req, res) =>
 	var select_promise = new Promise(function (resolve0, reject0){
 		var rows;
 			pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			client.query('SELECT password FROM users WHERE name = $1 ;', [dane.nazwa], function(err, result) {
+			client.query('SELECT (name, password) FROM users WHERE name = $1 ;', [dane.nazwa], function(err, result) {
 			done();
 			if (err)
 			{ console.error(err); res.send("Error " + err); reject0(true); }
@@ -268,7 +268,7 @@ app.post('/logIn', (req, res) =>
 				console.log(hash(dane.haslo))
 				if (resolve0[0].password == hash(dane.haslo))
 				{
-					req.session.username = resolve0[0].name
+					req.session.username = resolve[0].name
 					res.redirect('/')
 				}
 				else 
